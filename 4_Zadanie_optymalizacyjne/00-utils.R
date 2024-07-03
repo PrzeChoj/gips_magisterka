127156a62341948d2eb843b8a4c940b5f52678e2
PrzeChoj <premysl.choj@gmail.com>
2024-07-03 17:13:55 +0200

    No stabilizer

--- 4_Zadanie_optymalizacyjne/00-utils.R
+++ 4_Zadanie_optymalizacyjne/00-utils.R
@@ -2,27 +2,22 @@
 # Make true matrices
 stabilizer <- 0.1
 
+#' true_perm <- gips::gips_perm("(12345)", 10)
+#' A <- get_sigma_wishart_perm(true_perm, 2022)
+#' gips:::pretty_plot_matrix(A)
 get_sigma_wishart_perm <- function(perm, my_seed) {
   p <- attr(perm, "size")
   Z <- withr::with_seed(my_seed,
-                        code = MASS::mvrnorm(n = p, mu = numeric(p), Sigma = diag(1, p))
+     code = MASS::mvrnorm(n = p, mu = numeric(p), Sigma = diag(1, p))
   )
   sigma_wishart <- (t(Z) %*% Z) / p
   project_matrix(sigma_wishart, perm)
 }
 
-change_bigest_eigen_value <- function(A, stabilizer) {
-  r <- eigen(A, TRUE)
-  r$values[1] <- stabilizer
-  r$vectors %*% diag(r$values) %*% t(r$vectors)
-}
-
-true_matrix_generator <- function(my_seed, true_perm, stabilizer) {
-  change_bigest_eigen_value(get_sigma_wishart_perm(true_perm, my_seed), stabilizer)
-}
-
-S_matrix_generator <- function(my_seed, true_perm, stabilizer, n) {
-  true_matrix <- true_matrix_generator(my_seed, true_perm, stabilizer)
+#' true_perm <- gips::gips_perm("(12345)", 10)
+#' S_matrix_generator(1234, true_perm, 15)
+S_matrix_generator <- function(my_seed, true_perm, n) {
+  true_matrix <- get_sigma_wishart_perm(true_perm, my_seed)
   p <- attr(true_perm, "size")
   Z <- withr::with_seed(my_seed,
     code = MASS::mvrnorm(n = n, mu = numeric(p), Sigma = true_matrix)
