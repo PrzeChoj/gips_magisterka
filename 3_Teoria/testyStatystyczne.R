b169e620fb7e3000715a6fa7f0a80130d178f2f2
PrzeChoj <premysl.choj@gmail.com>
2024-07-13 16:57:45 +0200

    3/testy Better plot

--- 3_Teoria/testyStatystyczne.R
+++ 3_Teoria/testyStatystyczne.R
@@ -31,13 +31,23 @@ get_lambda <- function(my_seed, n, show_progress_bar = FALSE) {
   lambda_LR
 }
 
-my_plot <- function(my_lambdas, p_value_text_place) {
-  plot(ecdf(my_lambdas), xlim = c(0, 50), main = paste0("Wykres ECDF dla n = ", n))
+my_plot <- function(my_lambdas, p_value_text_place, legend_place) {
+  my_ecdf <- ecdf(my_lambdas)
+  plot(my_ecdf, xlim = c(0, 50), main = paste0("Wykres ECDF dla n = ", n), axes = FALSE)
+  axis(2, at = c(seq(0, 1, by = 0.2), 0.95), las=2)
+  box()
+  abline(h = 0.95, col = "gray")
   lines(0:M / (M/50), y = pchisq(0:M / (M/50), p*(p+1)/2 - 4), col = "red")
-  legend(25, 0.6, legend=c("chi^2_17", "lambda_LR"), col=c("red", "black"), lty=c(1,1))
+  chisq_p_value <- qchisq(0.95, p*(p+1)/2 - 4)
+  abline(v = chisq_p_value)
+  i_p_value_ecdf <- which.max(my_ecdf(seq(0, 50, by = 0.1)) > 0.95) # first index of TRUE
+  ecdf_p_value <- seq(0, 50, by = 0.1)[i_p_value_ecdf]
+  abline(v = ecdf_p_value)
+  axis(1, at = c(seq(0, 50, by = 10), round(chisq_p_value, 1), ecdf_p_value), las=2)
+  legend(legend_place, 0.6, legend=c("chi^2_17", "lambda_LR"), col=c("red", "black"), lty=c(1, 1))
   my_p_val <- ks.test(my_lambdas, "pchisq", p*(p+1)/2 - 4)
   text(p_value_text_place, 0.1, paste("p-value =", format(my_p_val$p.value, digits = 2, scientific = FALSE)),
-       cex=1, pos=3,col="blue")
+       cex = 1, pos = 3, col = "blue")
 }
 
 library(parallel)
@@ -48,15 +58,15 @@ M <- 1000
 
 n <- 10
 my_lambdas <- unlist(mclapply(1:M, function(my_seed){get_lambda(my_seed, n, FALSE)}, mc.cores = available_cores))
-my_plot(my_lambdas, 29)
+my_plot(my_lambdas, 31.5, 28)
 # save as plots/test_n_10.png
 
 n <- 30
 my_lambdas <- unlist(mclapply(1:M, function(my_seed){get_lambda(my_seed, n, FALSE)}, mc.cores = available_cores))
-my_plot(my_lambdas, 36)
+my_plot(my_lambdas, 42, 33)
 # save as plots/test_n_30.png
 
 n <- 150
 my_lambdas <- unlist(mclapply(1:M, function(my_seed){get_lambda(my_seed, n, FALSE)}, mc.cores = available_cores))
-my_plot(my_lambdas, 30)
+my_plot(my_lambdas, 34.5, 30)
 # save as plots/test_n_150.png
